5eb5be4d75be98b4823b38f35197628785cc84d7
Alejo Gonzales Chaves <alejoagchaves@gmail.com>
2019-10-29 11:03:57 -0300

    change :new routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  resources :kitchen do
-    resources :review, only: [:new, :create]
-    resources :appointment, only: [:new, :create]
+  resources :kitchens do
+    resources :reviews, only: [:new, :create]
+    resources :appointments, only: [:new, :create]
   end
-  resources :appointment, only: [:show, :edit, :update, :destroy]
+  resources :appointments, only: [:show, :edit, :update, :destroy]
 
 end
