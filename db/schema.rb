1db957f35f9cfab8d6f695bd205308b2cb2e8256
Alejo Gonzales Chaves <alejoagchaves@gmail.com>
2019-10-29 11:37:50 -0300

    user story of kitchen is done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_203338) do
+ActiveRecord::Schema.define(version: 2019_10_29_141106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_10_28_203338) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_kitchens_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 2019_10_28_203338) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "kitchens", "users"
   add_foreign_key "reviews", "kitchens"
 end
