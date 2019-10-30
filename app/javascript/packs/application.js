173407f7b1a8a46d6559e731f76bcf49bcfda60c
Santiago Llapur <santillapur@gmail.com>
2019-10-30 13:22:14 -0300

    Geolocalization and autocomplete js added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 flatpickr(".datepicker", {
   defaultDate: "today",
@@ -9,3 +12,7 @@ flatpickr(".datepicker", {
   minDate: "today",
   maxDate: new Date().fp_incr(30)
 });
+
+initMapbox();
+
+initAutocomplete();
