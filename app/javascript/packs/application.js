04af61006aa15750be3dc2d048800260ab4721ca
Santiago Llapur <santillapur@gmail.com>
2019-11-01 14:26:46 -0300

    Added disabled dates in flatpicker when user appoints a kitchen

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,22 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 
-flatpickr(".datepicker", {
-  defaultDate: "today",
-  altInput: true,
-  dateFormat: "d.m.Y",
-  minDate: "today",
-  maxDate: new Date().fp_incr(30)
-});
+
+const kitchen = document.getElementById('kitchen');
+
+if (kitchen) {
+  const disableDates = JSON.parse(kitchen.dataset.disableDates)
+
+  flatpickr(".datepicker", {
+    defaultDate: "today",
+    altInput: true,
+    dateFormat: "d.m.Y",
+    minDate: "today",
+    maxDate: new Date().fp_incr(30),
+    disable: disableDates,
+  });
+}
+
 
 initMapbox();
 
